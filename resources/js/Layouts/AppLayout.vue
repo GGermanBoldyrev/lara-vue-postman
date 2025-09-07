13ac1c89feb83c679e1846e4ee2e888fcfdc8e8e
German Boldyrev <boldyrev_german@mail.ru>
2025-09-07 07:44:22 +0300

    feat: base vue struct

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col bg-gray-100">
+        <!-- Хедер -->
+        <header class="bg-white shadow p-4">
+            <h1 class="text-xl font-bold text-gray-700">🚀 My Inertia + Vue App</h1>
+        </header>
+
+        <!-- Контент -->
+        <main class="flex-1 p-6">
+            <slot />
+        </main>
+
+        <!-- Футер -->
+        <footer class="bg-white border-t p-4 text-center text-sm text-gray-500">
+            &copy; {{ new Date().getFullYear() }} MyApp. Все права защищены.
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+</script>
+
+<template>
+    <AppLayout>
+        <div class="text-center py-10">
+            <h1 class="text-4xl font-bold mb-4">ghbdtn</h1>
+            <p class="text-lg text-gray-600">gjrf</p>
+        </div>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
